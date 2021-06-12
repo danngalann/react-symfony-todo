609d1b7181dbf6723d91c7659fac30ffc2269217
danngalann <47415745+danngalann@users.noreply.github.com>
2021-06-12 10:36:33 +0200

    Created table with material

--- assets/js/components/TodoTable.js
+++ assets/js/components/TodoTable.js
@@ -1,17 +1,56 @@
 import React, {useContext} from 'react';
 import {TodoContext} from "../context/TodoContext";
+import {IconButton, Table, TableBody, TableCell, TableHead, TableRow, TextField} from "@material-ui/core";
+
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function TodoTable() {
     const context = useContext(TodoContext);
 
     return (
-        <ul>
-            {
-                context.todos.map(todo => {
-                    return <li>{todo.title}</li>
-                })
-            }
-        </ul>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>
+                        Task
+                    </TableCell>
+                    <TableCell align="right">
+                        Actions
+                    </TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        <TextField fullWidth={true} />
+                    </TableCell>
+                    <TableCell  align="right">
+                        <IconButton>
+                            <AddCircleIcon />
+                        </IconButton>
+                    </TableCell>
+                </TableRow>
+                {
+                    context.todos.map(todo => (
+                        <TableRow>
+                            <TableCell>
+                                {todo.title}
+                            </TableCell>
+                            <TableCell align="right">
+                                <IconButton>
+                                    <EditIcon />
+                                </IconButton>
+                                <IconButton>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
+                        </TableRow>
+                    ))
+                }
+            </TableBody>
+        </Table>
     );
 }
 
